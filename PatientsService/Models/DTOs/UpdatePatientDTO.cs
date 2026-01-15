e81c6f3553098515254cd38bb46ce1d43a2b80ea
Julie B <julie.boulland@gmail.com>
2026-01-15 11:39:33 +0100

    Modification de la validation des champs pour add et update un patient

--- PatientsService/Models/DTOs/UpdatePatientDTO.cs
+++ PatientsService/Models/DTOs/UpdatePatientDTO.cs
@@ -1,11 +1,21 @@
-﻿namespace PatientsService.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientsService.Models.DTOs
 {
     public class UpdatePatientDTO
     {
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
+        [Required, StringLength(100, MinimumLength = 1)]
+        public string FirstName { get; set; } = "";
+
+        [Required, StringLength(100, MinimumLength = 1)]
+        public string LastName { get; set; } = "";
+
+        [Required]
         public DateOnly DateOfBirth { get; set; }
+
+        [Required]
         public string Gender { get; set; } = null!;
+
         public string? Address { get; set; } = null!;
         public string? PhoneNumber { get; set; } = null!;
     }
--- PatientsService/Models/Patient.cs
+++ PatientsService/Models/Patient.cs
@@ -1,32 +1,13 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-
-namespace Back.Models
+﻿namespace Back.Models
 {
     public class Patient
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Le prénom du patient est obligatoire")]
-        [DisplayName("Prénom")]
-        public string FirstName { get; set; } = "";
-
-        [Required(ErrorMessage = "Le nom du patient est obligatoire")]
-        [DisplayName("Nom")]
-        public string LastName { get; set; } = "";
-
-        [Required(ErrorMessage = "La date de naissance du patient est obligatoire")]
-        [DisplayName("Date de naissance")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateOnly DateOfBirth { get; set; }
-
-        [Required(ErrorMessage = "Le genre du patient est obligatoire")]
-        [DisplayName("Genre")]
-        public string Gender { get; set; } = "";
-
-        [DisplayName("Adresse")]
+        public string Gender { get; set; }
         public string? Address { get; set; }
-
-        [DisplayName("Numéro de téléphone")]
         public string? PhoneNumber { get; set; }
     }
 }
